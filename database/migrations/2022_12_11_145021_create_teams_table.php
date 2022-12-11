dc0df9ba8bd8537964c5e34865d843113f9b1167
NatalijaPl <pl.natalija95l@gmail.com>
2022-12-11 16:21:34 +0100

    zadatak 1

---
+++ database/migrations/2022_12_11_145021_create_teams_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *id, name, email, address, city
+
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+
+            $table->increments('id');
+
+            $table->string('name');
+            $table->string('email');
+            $table->string('adress');
+            $table->string('city');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+};
